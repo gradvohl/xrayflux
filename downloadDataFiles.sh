779633d1b9dd565eb952221020228316d1b8abb4
gradvohl <gradvohl@ft.unicamp.br>
2017-10-23 21:56:44 -0200

    Add download script

--- downloadDataFiles.sh
+++ downloadDataFiles.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Script to download datasets with X-ray fluxes from the Sun.
 # The user should provide a file with a list of datasets to download.
+# The script download at the current directory.
 #
 # Autor: Andre Leon S. Gradvohl, Dr.
 # Last version: Seg 23 Out 2017 21:41:47 -02
-#
 
 # Number of simultaneous connections
 NUMCONN=8
 
+# Check the number of arguments. Only one argument is necessary
 if [[ $# -ne 1 ]]; then
   echo "Wrong number of arguments."
+  echo -e "Usage:\n\t $0 <file with a list of datasets>" 
   exit 1
 fi
 
+#Check if the first argument is a file.
 if [[ -f $1 ]]; then
   FILE=$1
 else
@@ -22,12 +25,12 @@ else
 fi
 
 LINE=" "
+
 # Get the list of files that will be downloaded.
 for LINE in `cat ${FILE}`; do
   LIST=${LIST}" "${LINE}
 done
 
-#
 # Create NUMCONN parallel conections with wget.
 echo ${LIST} | xargs -n 1 -P ${NUMCONN} wget -q --no-check-certificate 
 
